87c76e5160a199b671b4072f94b386f5fd870157
Your Name <you@example.com>
2022-03-04 14:17:13 +0530

    navbar added

--- src/structure/Header.js
+++ src/structure/Header.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import icecream from "../assets/img/ultimate-ice-cream.svg";
 
 const Header = () => {
@@ -8,6 +10,13 @@ const Header = () => {
         <img src={icecream} alt="" />
         Awesome icecream
       </h1>
+      <nav>
+        <ul>
+          <NavLink to="/" exact>
+            Home Menu
+          </NavLink>
+        </ul>
+      </nav>
     </header>
   );
 };
